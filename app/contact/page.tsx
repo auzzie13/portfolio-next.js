782e6e5b95d0cd6c1dcfe1e520f1af45a0179873
Austin Williams <auswil13@gmail.com>
2024-02-08 21:17:11 -0600

    handle form submission

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,21 +1,39 @@
 'use client'; 
 
 import { Button, Flex, TextArea, TextField } from '@radix-ui/themes';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import React from 'react'
+import { useRouter } from 'next/navigation';
+
+interface MessageForm {
+  first_name: string;
+  last_name: string;
+  email: string;
+  message: string;
+}
 
 const ContactPage = () => {
+  const router = useRouter();
+  const {register, handleSubmit} = useForm<MessageForm>();
+
   return (
-    <div className='pt-4 text-red-800 bg-stone-800 mx-2 h-screen space-y-3'>
+    <form 
+      className='pt-4 text-red-800 bg-stone-800 mx-2 h-screen space-y-3' 
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/messages', data);
+        router.push('/');
+      })}>
 
     <Flex className='mx-auto w-1/2' gap='3'>
-      <TextField.Input placeholder='First Name' />
-      <TextField.Input placeholder='Last Name' />
-      <TextField.Input placeholder='Email' />
+      <TextField.Input placeholder='First Name' {...register('first_name')} />
+      <TextField.Input placeholder='Last Name' {...register('last_name')} />
+      <TextField.Input placeholder='Email' {...register('email')} />
     </Flex>
-    <TextArea style={{ width: '50%', marginRight: 'auto', marginLeft: 'auto' }} placeholder='Write me a message...' />
+    <TextArea style={{ width: '50%', marginRight: 'auto', marginLeft: 'auto' }} placeholder='Write me a message...' {...register('message')} />
     <Button>Send Message</Button>
 
-    </div>
+    </form>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <Theme>
         <NavBar />
         <main>{children}</main>
-        <SideBar />
+        {/* <SideBar /> */}
         </Theme>
       </body>
     </html>
