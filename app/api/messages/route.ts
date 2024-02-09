9bf85400cb18f86e47f59e1df537d4abeca3f0ac
Austin Williams <auswil13@gmail.com>
2024-02-08 21:38:40 -0600

    handle errors

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -3,10 +3,10 @@ import { z } from 'zod';
 import prisma from "@/prisma/client";
 
 const createMessageSchema = z.object({
-    first_name: z.string().min(1).max(50),
-    last_name: z.string().min(1).max(50),
-    email: z.string().min(1).max(50),
-    message: z.string().min(1)
+    first_name: z.string().min(1, "First name is required.").max(50),
+    last_name: z.string().min(1, "Last name is required.").max(50),
+    email: z.string().min(1, "Email is required.").max(50),
+    message: z.string().min(1, "Your message is empty.")
 })
 
 
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,9 @@
 'use client'; 
 
-import { Button, Flex, TextArea, TextField } from '@radix-ui/themes';
+import { Button, Callout, Flex, TextArea, TextField } from '@radix-ui/themes';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
 
 interface MessageForm {
@@ -16,13 +16,22 @@ interface MessageForm {
 const ContactPage = () => {
   const router = useRouter();
   const {register, handleSubmit} = useForm<MessageForm>();
+  const [error, setError] = useState('');
 
   return (
+    <div className='pt-4  bg-stone-800 mx-2 h-screen'>
+      {error && <Callout.Root color='red'>
+        <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>}
     <form 
-      className='pt-4 text-red-800 bg-stone-800 mx-2 h-screen space-y-3' 
+      className='pt-4 space-y-3' 
       onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/messages', data);
-        router.push('/');
+        try {
+          await axios.post('/api/messages', data);
+          router.push('/');          
+        } catch (error) {
+          setError('An unexpected error occurred.')
+        }
       })}>
 
     <Flex className='mx-auto w-1/2' gap='3'>
@@ -34,6 +43,7 @@ const ContactPage = () => {
     <Button>Send Message</Button>
 
     </form>
+    </div>
   )
 }
 
